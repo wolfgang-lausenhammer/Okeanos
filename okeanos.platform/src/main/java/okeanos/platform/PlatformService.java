dd9c025e683a6d7938e1844aeb070a57fca51170
wolfgang-lausenhammer <wolfgang.lausenhammer@gmail.com>
2014-02-19 09:32:54 -0500

    Created basic project structure

---
+++ okeanos.platform/src/main/java/okeanos/platform/PlatformService.java
@@ -0,0 +1,12 @@
+package okeanos.platform;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface PlatformService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
